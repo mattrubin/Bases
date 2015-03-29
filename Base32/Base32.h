df67a0485bbefd88b408e7428c0d957497d91fd8
Matt Rubin <opensource@mattrubin.me>
2015-03-29 00:05:41 -0400

    Add Base32 framework target

---
+++ Base32/Base32.h
@@ -0,0 +1,19 @@
+//
+//  Base32.h
+//  Base32
+//
+//  Created by Matt Rubin on 3/28/15.
+//  Copyright (c) 2015 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Base32.
+FOUNDATION_EXPORT double Base32VersionNumber;
+
+//! Project version string for Base32.
+FOUNDATION_EXPORT const unsigned char Base32VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base32/PublicHeader.h>
+
+
