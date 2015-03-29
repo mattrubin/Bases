8e89afad53606b59ee3e9c6f75559c65ab36d2b2
Matt Rubin <opensource@mattrubin.me>
2015-03-29 19:21:12 -0400

    Consolidate all cases

--- Base32/Base32.swift
+++ Base32/Base32.swift
@@ -45,12 +45,8 @@ private func encodeQuantum(bytes: ArraySlice<UInt8>) -> String? {
         // The final quantum of encoding input is exactly 8 bits; here, the
         // final unit of encoded output will be two characters followed by
         // six "=" padding characters.
-        let q = quintets(bytes[0])
-        if let
-            c0 = characterForValue(q.0),
-            c1 = characterForValue(q.1)
-        {
-            return String([c0, c1, pad, pad, pad, pad, pad, pad])
+        if let c = charactersForBytes(bytes[0], nil, nil, nil, nil) {
+            return String([c.0, c.1, c.2, c.3, c.4, c.5, c.6, c.7])
         }
     case 2:
         // The final quantum of encoding input is exactly 16 bits; here, the
@@ -87,15 +83,15 @@ private func encodeQuantum(bytes: ArraySlice<UInt8>) -> String? {
 }
 
 
-private func charactersForBytes(b0: UInt8, b1: UInt8, b2: UInt8?, b3: UInt8?, b4: UInt8?)
+private func charactersForBytes(b0: UInt8, b1: UInt8?, b2: UInt8?, b3: UInt8?, b4: UInt8?)
     -> (Character, Character, Character, Character, Character, Character, Character, Character)?
 {
     let q = quintets(b0, b1, b2, b3, b4)
     if let
         c0 = characterForValue(q.0),
         c1 = characterForValue(q.1),
-        c2 = characterForValue(q.2),
-        c3 = characterForValue(q.3),
+        c2 = characterOrPaddingForValue(q.2),
+        c3 = characterOrPaddingForValue(q.3),
         c4 = characterOrPaddingForValue(q.4),
         c5 = characterOrPaddingForValue(q.5),
         c6 = characterOrPaddingForValue(q.6),
--- Base32/Quintets.swift
+++ Base32/Quintets.swift
@@ -6,23 +6,14 @@
 //  Copyright (c) 2015 Matt Rubin. All rights reserved.
 //
 
-func quintets(b0: UInt8)
-    -> (UInt8, UInt8)
+func quintets(b0: UInt8, b1: UInt8?, b2: UInt8?, b3: UInt8?, b4: UInt8?)
+    -> (UInt8, UInt8, UInt8?, UInt8?, UInt8?, UInt8?, UInt8?, UInt8?)
 {
     return (
         firstQuintet(b0),
-        secondQuintet(b0, 0)
-    )
-}
-
-func quintets(b0: UInt8, b1: UInt8, b2: UInt8?, b3: UInt8?, b4: UInt8?)
-    -> (UInt8, UInt8, UInt8, UInt8, UInt8?, UInt8?, UInt8?, UInt8?)
-{
-    return (
-        firstQuintet(b0),
-        secondQuintet(b0, b1),
-        thirdQuintet(b1),
-        fourthQuintet(b1, b2 ?? 0),
+        secondQuintet(b0, b1 ?? 0),
+        b1.map(thirdQuintet),
+        b1.map(fourthQuintet)?(b2 ?? 0),
         b2.map(fifthQuintet)?(b3 ?? 0),
         b3.map(sixthQuintet),
         b3.map(seventhQuintet)?(b4 ?? 0),
@@ -45,7 +36,7 @@ private func thirdQuintet(b1: UInt8) -> UInt8 {
     return ((b1 & 0b00111110) >> 1)
 }
 
-private func fourthQuintet(b1: UInt8, b2: UInt8) -> UInt8 {
+private func fourthQuintet(b1: UInt8)(_ b2: UInt8) -> UInt8 {
     return ((b1 & 0b00000001) << 4)
         |  ((b2 & 0b11110000) >> 4)
 }
