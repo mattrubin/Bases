34f3503440500881a123377f5a21fce95d026ecc
Matt Rubin <opensource@mattrubin.me>
2015-03-29 02:53:15 -0400

    Don't import UIKit

--- Base32/Base32.h
+++ Base32/Base32.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Matt Rubin. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for Base32.
 FOUNDATION_EXPORT double Base32VersionNumber;
