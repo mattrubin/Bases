13f8a0d37df9aa5df84a84223b21dd3c9342c5f9
Matt Rubin <opensource@mattrubin.me>
2015-03-29 17:58:20 -0400

    Add comments from the RFC

--- Base32/Base32.swift
+++ Base32/Base32.swift
@@ -26,10 +26,21 @@ public func encode(bytes: ArraySlice<UInt8>) -> String? {
 }
 
 private func encodeQuantum(bytes: ArraySlice<UInt8>) -> String? {
+    // Special processing is performed if fewer than 40 bits are available
+    // at the end of the data being encoded.  A full encoding quantum is
+    // always completed at the end of a body.  When fewer than 40 input bits
+    // are available in an input group, bits with value zero are added (on
+    // the right) to form an integral number of 5-bit groups.  Padding at
+    // the end of the data is performed using the "=" character.  Since all
+    // base 32 input is an integral number of octets, only the following
+    // cases can arise:
     switch bytes.count {
     case 0:
         return ""
     case 1:
+        // The final quantum of encoding input is exactly 8 bits; here, the
+        // final unit of encoded output will be two characters followed by
+        // six "=" padding characters.
         let q = quintets(bytes[0])
         if let
             c0 = characterForValue(q.0),
@@ -38,6 +49,9 @@ private func encodeQuantum(bytes: ArraySlice<UInt8>) -> String? {
             return String([c0, c1, "=", "=", "=", "=", "=", "="])
         }
     case 2:
+        // The final quantum of encoding input is exactly 16 bits; here, the
+        // final unit of encoded output will be four characters followed by
+        // four "=" padding characters.
         let q = quintets(bytes[0], bytes[1])
         if let
             c0 = characterForValue(q.0),
@@ -48,6 +62,9 @@ private func encodeQuantum(bytes: ArraySlice<UInt8>) -> String? {
             return String([c0, c1, c2, c3, "=", "=", "=", "="])
         }
     case 3:
+        // The final quantum of encoding input is exactly 24 bits; here, the
+        // final unit of encoded output will be five characters followed by
+        // three "=" padding characters.
         let q = quintets(bytes[0], bytes[1], bytes[2])
         if let
             c0 = characterForValue(q.0),
@@ -59,6 +76,9 @@ private func encodeQuantum(bytes: ArraySlice<UInt8>) -> String? {
             return String([c0, c1, c2, c3, c4, "=", "=", "="])
         }
     case 4:
+        // The final quantum of encoding input is exactly 32 bits; here, the
+        // final unit of encoded output will be seven characters followed by
+        // one "=" padding character.
         let q = quintets(bytes[0], bytes[1], bytes[2], bytes[3])
         if let
             c0 = characterForValue(q.0),
@@ -72,6 +92,9 @@ private func encodeQuantum(bytes: ArraySlice<UInt8>) -> String? {
             return String([c0, c1, c2, c3, c4, c5, c6, "="])
         }
     default:
+        // The final quantum of encoding input is an integral multiple of 40
+        // bits; here, the final unit of encoded output will be an integral
+        // multiple of 8 characters with no "=" padding.
         let q = quintets(bytes[0], bytes[1], bytes[2], bytes[3], bytes[4])
         if let
             c0 = characterForValue(q.0),
@@ -86,6 +109,7 @@ private func encodeQuantum(bytes: ArraySlice<UInt8>) -> String? {
             return String([c0, c1, c2, c3, c4, c5, c6, c7])
         }
     }
+
     // Something failed
     return nil
 }
