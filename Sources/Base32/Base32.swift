52537955ed11e4bf1611bb30933bb49b0915034e
Matt Rubin <opensource@mattrubin.me>
2016-04-26 22:35:18 -0400

    Overload stringForBytes(...) for different byte counts

--- Sources/Base32/Base32.swift
+++ Sources/Base32/Base32.swift
@@ -28,23 +28,19 @@ private func stringForNextQuantum(bytes: ArraySlice<Byte>) -> String {
     case 0:
         return ""
     case 1:
-        return stringForBytes(bytes[bytes.startIndex],
-                              nil, nil, nil, nil)
+        return stringForBytes(bytes[bytes.startIndex])
     case 2:
         return stringForBytes(bytes[bytes.startIndex],
-                              bytes[bytes.startIndex + 1],
-                              nil, nil, nil)
+                              bytes[bytes.startIndex + 1])
     case 3:
         return stringForBytes(bytes[bytes.startIndex],
                               bytes[bytes.startIndex + 1],
-                              bytes[bytes.startIndex + 2],
-                              nil, nil)
+                              bytes[bytes.startIndex + 2])
     case 4:
         return stringForBytes(bytes[bytes.startIndex],
                               bytes[bytes.startIndex + 1],
                               bytes[bytes.startIndex + 2],
-                              bytes[bytes.startIndex + 3],
-                              nil)
+                              bytes[bytes.startIndex + 3])
     default:
         return stringForBytes(bytes[bytes.startIndex],
                               bytes[bytes.startIndex + 1],
@@ -54,9 +50,60 @@ private func stringForNextQuantum(bytes: ArraySlice<Byte>) -> String {
     }
 }
 
-private func stringForBytes(_ b0: Byte, _ b1: Byte?, _ b2: Byte?, _ b3: Byte?, _ b4: Byte?) -> String
-{
+private func stringForBytes(_ b0: Byte, _ b1: Byte, _ b2: Byte, _ b3: Byte, _ b4: Byte) -> String {
     let q = quintetsFromBytes(b0, b1, b2, b3, b4)
+    let c0 = characterForValue(q.0)
+    let c1 = characterForValue(q.1)
+    let c2 = characterOrPaddingForValue(q.2)
+    let c3 = characterOrPaddingForValue(q.3)
+    let c4 = characterOrPaddingForValue(q.4)
+    let c5 = characterOrPaddingForValue(q.5)
+    let c6 = characterOrPaddingForValue(q.6)
+    let c7 = characterOrPaddingForValue(q.7)
+    return String([c0, c1, c2, c3, c4, c5, c6, c7])
+}
+
+private func stringForBytes(_ b0: Byte, _ b1: Byte, _ b2: Byte, _ b3: Byte) -> String {
+    let q = quintetsFromBytes(b0, b1, b2, b3, nil)
+    let c0 = characterForValue(q.0)
+    let c1 = characterForValue(q.1)
+    let c2 = characterOrPaddingForValue(q.2)
+    let c3 = characterOrPaddingForValue(q.3)
+    let c4 = characterOrPaddingForValue(q.4)
+    let c5 = characterOrPaddingForValue(q.5)
+    let c6 = characterOrPaddingForValue(q.6)
+    let c7 = characterOrPaddingForValue(q.7)
+    return String([c0, c1, c2, c3, c4, c5, c6, c7])
+}
+
+private func stringForBytes(_ b0: Byte, _ b1: Byte, _ b2: Byte) -> String {
+    let q = quintetsFromBytes(b0, b1, b2, nil, nil)
+    let c0 = characterForValue(q.0)
+    let c1 = characterForValue(q.1)
+    let c2 = characterOrPaddingForValue(q.2)
+    let c3 = characterOrPaddingForValue(q.3)
+    let c4 = characterOrPaddingForValue(q.4)
+    let c5 = characterOrPaddingForValue(q.5)
+    let c6 = characterOrPaddingForValue(q.6)
+    let c7 = characterOrPaddingForValue(q.7)
+    return String([c0, c1, c2, c3, c4, c5, c6, c7])
+}
+
+private func stringForBytes(_ b0: Byte, _ b1: Byte) -> String {
+    let q = quintetsFromBytes(b0, b1, nil, nil, nil)
+    let c0 = characterForValue(q.0)
+    let c1 = characterForValue(q.1)
+    let c2 = characterOrPaddingForValue(q.2)
+    let c3 = characterOrPaddingForValue(q.3)
+    let c4 = characterOrPaddingForValue(q.4)
+    let c5 = characterOrPaddingForValue(q.5)
+    let c6 = characterOrPaddingForValue(q.6)
+    let c7 = characterOrPaddingForValue(q.7)
+    return String([c0, c1, c2, c3, c4, c5, c6, c7])
+}
+
+private func stringForBytes(_ b0: Byte) -> String {
+    let q = quintetsFromBytes(b0, nil, nil, nil, nil)
     let c0 = characterForValue(q.0)
     let c1 = characterForValue(q.1)
     let c2 = characterOrPaddingForValue(q.2)
