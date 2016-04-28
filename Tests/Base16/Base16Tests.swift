6d042b67b470a552205e62c3c4523f9c87e2b16c
Matt Rubin <opensource@mattrubin.me>
2016-04-28 02:14:09 -0400

    Clean up Base16 tests

--- Tests/Base16/Base16Tests.swift
+++ Tests/Base16/Base16Tests.swift
@@ -10,36 +10,26 @@ import XCTest
 import Base16
 
 class Base16Tests: XCTestCase {
-    let rfcTestVectors = [("", ""),
-                          ("f", "66"),
-                          ("fo", "666F"),
-                          ("foo", "666F6F"),
-                          ("foob", "666F6F62"),
-                          ("fooba", "666F6F6261"),
-                          ("foobar", "666F6F626172")]
-
     func testRFC() {
-        for (sourceString, encodedString) in rfcTestVectors {
-            assert(ASCII: sourceString, encodesTo: encodedString)
-            assert(encodedString: encodedString, decodesToASCII: sourceString)
-        }
-    }
+        let rfcTestVectors = [("", ""),
+                              ("f", "66"),
+                              ("fo", "666F"),
+                              ("foo", "666F6F"),
+                              ("foob", "666F6F62"),
+                              ("fooba", "666F6F6261"),
+                              ("foobar", "666F6F626172")]
 
-    private func assert(ASCII sourceString: String, encodesTo encodedString: String, file: StaticString = #file, line: UInt = #line) {
-        if let data = sourceString.data(using: NSASCIIStringEncoding) {
-            let result = Base16.encode(data: data)
-            XCTAssertEqual(result, encodedString, "ASCII string \"\(sourceString)\" encoded to \"\(result)\" (expected result: \"\(encodedString)\")", file: file, line: line)
-        } else {
-            XCTFail("Could not convert ASCII string \"\(sourceString)\" to NSData", file: file, line: line)
-        }
-    }
+        for (decodedString, encodedString) in rfcTestVectors {
+            guard let decodedData = decodedString.data(using: NSASCIIStringEncoding) else {
+                XCTFail("Could not convert ASCII string \"\(decodedString)\" to NSData")
+                continue
+            }
 
-    private func assert(encodedString: String, decodesToASCII sourceString: String, file: StaticString = #file, line: UInt = #line) {
-        let data = Base16.decode(string: encodedString)!
-        if let result = String(data: data, encoding: NSASCIIStringEncoding) {
-            XCTAssertEqual(result, sourceString, "Encoded string \"\(encodedString)\" decoded to ASCII string \"\(result)\" (expected result: \"\(sourceString)\")", file: file, line: line)
-        } else {
-            XCTFail("Could not convert decoded data to ASCII string", file: file, line: line)
+            let encodedResult = Base16.encode(data: decodedData)
+            XCTAssertEqual(encodedResult, encodedString, "ASCII string \"\(decodedString)\" encoded to \"\(encodedResult)\" (expected \"\(encodedString)\")")
+
+            let decodedResult = Base16.decode(string: encodedString)!
+            XCTAssertEqual(decodedResult, decodedData, "Encoded string \"\(encodedString)\" decoded to data \"\(decodedResult)\" (expected \"\(decodedData)\")")
         }
     }
 }
