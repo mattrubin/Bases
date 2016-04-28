6951ab07b6244913a17f69f5f1b9e72881babb81
Matt Rubin <opensource@mattrubin.me>
2016-04-28 01:59:41 -0400

    Implement Base16 decoding

--- Sources/Base16/Base16.swift
+++ Sources/Base16/Base16.swift
@@ -19,6 +19,16 @@ public enum Base16 {
 
     private static let encodingTable: [EncodedChar] = [48, 49, 50, 51, 52, 53, 54, 55,
                                                        56, 57, 65, 66, 67, 68, 68, 70]
+    private static let __: Byte? = nil
+    private static let decodingTable: [Byte?] =
+        [__, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __,
+         __, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __,
+         __, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __,
+          0,  1,  2,  3,  4,  5,  6,  7,  8,  9, __, __, __, __, __, __,
+         __, 10, 11, 12, 13, 14, 15, __, __, __, __, __, __, __, __, __,
+         __, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __,
+         __, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __,
+         __, __, __, __, __, __, __, __, __, __, __, __, __, __, __, __]
 
     public static func encode(data: NSData) -> String {
         let unencodedLength = data.length
@@ -45,5 +55,43 @@ public enum Base16 {
         let encodedData = NSData(bytesNoCopy: encodedBytes, length: encodedLength)
         let encodedString = String(data: encodedData, encoding: NSASCIIStringEncoding)!
         return encodedString
+    }
+
+    public static func decode(string: String) -> NSData? {
+        guard let encodedData = string.data(using: NSASCIIStringEncoding) else {
+            return nil
+        }
+        let encodedLength = encodedData.length
+        let encodedBytes = UnsafePointer<Byte>(encodedData.bytes)
+
+        guard let decodedLength = unencodedLength(encodedLength: encodedLength) else {
+            return nil
+        }
+        let decodedBytes = UnsafeMutablePointer<Byte>(allocatingCapacity: decodedLength)
+
+        var decodedWriteOffset = 0
+        for encodedReadOffset in stride(from: 0, to: encodedLength, by: encodedBlockSize) {
+            let bigChar = encodedBytes[encodedReadOffset]
+            let littleChar = encodedBytes[encodedReadOffset + 1]
+
+            let bigNibble = decodingTable[Int(bigChar)]!
+            let littleNibble = decodingTable[Int(littleChar)]!
+
+            let decodedByte = ((bigNibble & 0b00001111) << 4) | (littleNibble & 0b00001111)
+            decodedBytes[decodedWriteOffset] = decodedByte
+
+            decodedWriteOffset += unencodedBlockSize
+        }
+
+        // The NSData object takes ownership of the allocated bytes and will handle deallocation.
+        let decodedData = NSData(bytesNoCopy: decodedBytes, length: decodedLength)
+        return decodedData
+    }
+
+    private static func unencodedLength(encodedLength: Int) -> Int? {
+        guard encodedLength % encodedBlockSize == 0 else {
+            return nil
+        }
+        return (encodedLength / encodedBlockSize) * unencodedBlockSize
     }
 }
--- Tests/Base16/Base16Tests.swift
+++ Tests/Base16/Base16Tests.swift
@@ -10,14 +10,19 @@ import XCTest
 import Base16
 
 class Base16Tests: XCTestCase {
+    let rfcTestVectors = [("", ""),
+                          ("f", "66"),
+                          ("fo", "666F"),
+                          ("foo", "666F6F"),
+                          ("foob", "666F6F62"),
+                          ("fooba", "666F6F6261"),
+                          ("foobar", "666F6F626172")]
+
     func testRFC() {
-        assert(ASCII: "", encodesTo: "")
-        assert(ASCII: "f", encodesTo: "66")
-        assert(ASCII: "fo", encodesTo: "666F")
-        assert(ASCII: "foo", encodesTo: "666F6F")
-        assert(ASCII: "foob", encodesTo: "666F6F62")
-        assert(ASCII: "fooba", encodesTo: "666F6F6261")
-        assert(ASCII: "foobar", encodesTo: "666F6F626172")
+        for (sourceString, encodedString) in rfcTestVectors {
+            assert(ASCII: sourceString, encodesTo: encodedString)
+            assert(encodedString: encodedString, decodesToASCII: sourceString)
+        }
     }
 
     private func assert(ASCII sourceString: String, encodesTo encodedString: String, file: StaticString = #file, line: UInt = #line) {
@@ -26,6 +31,15 @@ class Base16Tests: XCTestCase {
             XCTAssertEqual(result, encodedString, "ASCII string \"\(sourceString)\" encoded to \"\(result)\" (expected result: \"\(encodedString)\")", file: file, line: line)
         } else {
             XCTFail("Could not convert ASCII string \"\(sourceString)\" to NSData", file: file, line: line)
+        }
+    }
+
+    private func assert(encodedString: String, decodesToASCII sourceString: String, file: StaticString = #file, line: UInt = #line) {
+        let data = Base16.decode(string: encodedString)!
+        if let result = String(data: data, encoding: NSASCIIStringEncoding) {
+            XCTAssertEqual(result, sourceString, "Encoded string \"\(encodedString)\" decoded to ASCII string \"\(result)\" (expected result: \"\(sourceString)\")", file: file, line: line)
+        } else {
+            XCTFail("Could not convert decoded data to ASCII string", file: file, line: line)
         }
     }
 }
