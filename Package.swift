a345a301c476190cec67d9815d06fa6bec47c00c
Matt Rubin <opensource@mattrubin.me>
2019-06-09 17:25:51 -0400

    Add the library and test targets to Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,24 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-    name: "Bases"
+    name: "Bases",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2),
+    ],
+    products: [
+        .library(name: "Base16", targets: ["Base16"]),
+        .library(name: "Base32", targets: ["Base32"]),
+    ],
+    targets: [
+        .target(name: "Base16"),
+        .target(name: "Base32"),
+        .testTarget(name: "Base16Tests", dependencies: ["Base16"]),
+        .testTarget(name: "Base32Tests", dependencies: ["Base32"]),
+    ],
+    swiftLanguageVersions: [.v5]
 )
