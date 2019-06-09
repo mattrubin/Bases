a5ea4909661ef803157668ae4922c4462a086f1f
Matt Rubin <opensource@mattrubin.me>
2019-06-09 17:10:46 -0400

    Update copyright dates

--- Sources/Base32/Base32.swift
+++ Sources/Base32/Base32.swift
@@ -2,7 +2,7 @@
 //  Base32.swift
 //  Bases
 //
-//  Copyright (c) 2015-2016 Matt Rubin and the Bases authors
+//  Copyright (c) 2015-2019 Matt Rubin and the Bases authors
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
--- Tests/Base32Tests/Base32Tests.swift
+++ Tests/Base32Tests/Base32Tests.swift
@@ -2,7 +2,7 @@
 //  Base32Tests.swift
 //  Base32Tests
 //
-//  Copyright (c) 2015-2016 Matt Rubin and the Bases authors
+//  Copyright (c) 2015-2018 Matt Rubin and the Bases authors
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
