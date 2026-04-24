789a8ce4299427a83392c4e507ca029956c1473f
Matt Rubin <opensource@mattrubin.me>
2026-04-24 10:49:07 -0400

    Migrate to Swift 6

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version:5.0
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "Bases",
     platforms: [
-        .iOS(.v8),
-        .macOS(.v10_10),
-        .tvOS(.v9),
-        .watchOS(.v2),
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .tvOS(.v12),
+        .watchOS(.v4),
     ],
     products: [
         .library(name: "Base16", targets: ["Base16"]),
@@ -20,7 +20,6 @@ let package = Package(
         .target(name: "Base32"),
         .testTarget(name: "Base16Tests", dependencies: ["Base16"]),
         .testTarget(name: "Base32Tests", dependencies: ["Base32"]),
-        .target(name: "PerformanceTests", dependencies: ["Base32"]),
+        .executableTarget(name: "PerformanceTests", dependencies: ["Base32"]),
     ],
-    swiftLanguageVersions: [.v5]
 )
